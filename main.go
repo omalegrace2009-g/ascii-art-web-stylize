7c01dab6fd62b7843ed16cbeaa8abddfe95b47b1
oomale <omalegrace2009@gmail.com>
2026-07-02 18:16:14 +0100

    progress

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ type PageData struct {
 }
 
 func main() {
+	serve:= http.Dir("static/")
+	get := http.StripPrefix("/static/", http.FileServer(serve))
+	http.Handle("/static/", get)
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/ascii-art", ArtHandler)
-	fmt.Println("Server Running:")
+	fmt.Println("Server Running on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
